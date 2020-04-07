f70523d8d9026d5e3c99d906168b62661dd53541
idanacumen <idan@acumen.io>
2020-04-07 11:19:33 +0300

    Extracted group_df_for_cm as a helper function

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -27,8 +27,16 @@ def sk_to_grouped_df(labels
             , classifier : predictions}
     df = DataFrame(dict)
     df = df.reset_index()
-    grouped_df = df.groupby([concept,classifier], as_index=False).agg({'index' : 'count'})
-    grouped_df = grouped_df.rename(columns={'index' : 'count'})
+    grouped_df = group_df_for_cm(df, classifier, concept)
+
+    return grouped_df
+
+
+def group_df_for_cm(df
+                    , classifier
+                    , concept):
+    grouped_df = df.groupby([concept, classifier], as_index=False).agg({'index': 'count'})
+    grouped_df = grouped_df.rename(columns={'index': 'count'})
 
     return grouped_df
 
