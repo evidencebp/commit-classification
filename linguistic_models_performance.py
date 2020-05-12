ba7674a1511c44a8c7146d1414d922d077b5941f
idanacumen <idan@acumen.io>
2020-05-12 15:11:51 +0300

    Move get fp/fn to analisys_utils

--- linguistic_models_performance.py
+++ linguistic_models_performance.py
@@ -60,16 +60,6 @@ def linguistic_model_perfomance(df
         adaptive_performance(df)
 
 
-def get_false_positives(df
-                        , classifier
-                        , concept):
-    return df[(df[classifier] == True) & (df[concept] == False)]
-
-def get_false_negatives(df
-                        , classifier
-                        , concept):
-    return df[(df[classifier] == False) & (df[concept] == True)]
-
 
 def main():
     print( "test performance")
