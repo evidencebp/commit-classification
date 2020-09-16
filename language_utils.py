b58a6125f7be6d471f5ce59e4765b0fc787d47e8
idanacumen <idan@acumen.io>
2020-09-16 16:21:23 +0300

    Extend file schema
    test performance
    ***********************************
    corrective commit performance
    {'true_positives': 225, 'true_negatives': 730, 'false_positives': 32, 'false_negatives': 43, 'samples': 1030, 'accuracy': 0.93, 'positive_rate': 0.26, 'hit_rate': 0.25, 'precision': 0.88, 'precision_lift': 2.36, 'recall': 0.84, 'fpr': 0.04, 'jaccard': 0.75, 'independent_prob': 0.62, 'lift_over_independent': 0.5, 'lift_over_majority': 0.25, 'comment': None}
    refactor commit performance
    {'true_positives': 82, 'true_negatives': 874, 'false_positives': 22, 'false_negatives': 52, 'samples': 1030, 'accuracy': 0.93, 'positive_rate': 0.13, 'hit_rate': 0.1, 'precision': 0.79, 'precision_lift': 5.06, 'recall': 0.61, 'fpr': 0.02, 'jaccard': 0.53, 'independent_prob': 0.8, 'lift_over_independent': 0.17, 'lift_over_majority': 0.07, 'comment': None}
    adaptive commit performance
    {'true_positives': 327, 'true_negatives': 334, 'false_positives': 115, 'false_negatives': 254, 'samples': 1030, 'accuracy': 0.64, 'positive_rate': 0.56, 'hit_rate': 0.43, 'precision': 0.74, 'precision_lift': 0.31, 'recall': 0.56, 'fpr': 0.26, 'jaccard': 0.47, 'independent_prob': 0.49, 'lift_over_independent': 0.31, 'lift_over_majority': 0.14, 'comment': None}
    perfective commit performance
    {'true_positives': 137, 'true_negatives': 746, 'false_positives': 73, 'false_negatives': 74, 'samples': 1030, 'accuracy': 0.86, 'positive_rate': 0.2, 'hit_rate': 0.2, 'precision': 0.65, 'precision_lift': 2.18, 'recall': 0.65, 'fpr': 0.09, 'jaccard': 0.48, 'independent_prob': 0.67, 'lift_over_independent': 0.27, 'lift_over_majority': 0.08, 'comment': None}
    validation performance
    ***********************************
    corrective commit performance
    {'true_positives': 92, 'true_negatives': 256, 'false_positives': 35, 'false_negatives': 17, 'samples': 400, 'accuracy': 0.87, 'positive_rate': 0.27, 'hit_rate': 0.32, 'precision': 0.72, 'precision_lift': 1.66, 'recall': 0.84, 'fpr': 0.12, 'jaccard': 0.64, 'independent_prob': 0.58, 'lift_over_independent': 0.49, 'lift_over_majority': 0.2, 'comment': None}

--- language_utils.py
+++ language_utils.py
@@ -36,7 +36,7 @@ documentation_entities = [
     'manual',
     'note(?:s)?',
     'readme(?:.md)?',
-    '[a-z\d]*.(md|txt)',
+    r'[a-z\d_\-/\\]*.(md|txt)',
     'translation(?:s)?',
     'java(?: |-)?doc(?:s)?',
     'java(?: |-)?documentation',
