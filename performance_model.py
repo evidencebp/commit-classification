37802ac3c316a3c7a3befcf774838f282cdf162a
idanacumen <idan@acumen.io>
2021-07-19 15:23:47 +0300

    Allow optimze arrengment

--- performance_model.py
+++ performance_model.py
@@ -46,6 +46,7 @@ excluded_terms = ['[a-z0-9/\.-]*fast/[a-z0-9/\.-]*',
                   'renam' + VERB_E_SUFFIX + NEAR_ENOUGH + 'fast',
                   'sometime(?:s)?',
                   '(unnoticed|found)' + NEAR_ENOUGH + 'long time',
+                  'optimize (align|alignment|arrange|arrangement)'
                   ]
 
 def build_positive_regex():
@@ -127,82 +128,7 @@ if __name__ == '__main__':
     print_concepts_functions_for_bq(commit='90a58fe07119d5c7ac094913e72305bf314dd65b')
     #evaluate_performance_classifier()
 
-    text = """
-"2009-02-23  Geoffrey Garen  <ggaren@apple.com>
-
-        Reviewed by Sam Weinig.
-
-        Next step in splitting JIT functionality out of the Interpreter class:
-        Moved vptr storage from Interpreter to JSGlobalData, so it could be shared
-        between Interpreter and JITStubs, and moved the *Trampoline JIT stubs
-        into the JITStubs class. Also added a VPtrSet class to encapsulate vptr
-        hacks during JSGlobalData initialization.
-        
-        SunSpider says 0.4% faster. Meh.
-
-        * JavaScriptCore.exp:
-        * JavaScriptCore.xcodeproj/project.pbxproj:
-        * interpreter/Interpreter.cpp:
-        (JSC::Interpreter::Interpreter):
-        (JSC::Interpreter::tryCacheGetByID):
-        (JSC::Interpreter::privateExecute):
-        * interpreter/Interpreter.h:
-        * jit/JIT.cpp:
-        (JSC::JIT::privateCompileMainPass):
-        (JSC::JIT::privateCompile):
-        (JSC::JIT::privateCompileCTIMachineTrampolines):
-        * jit/JIT.h:
-        (JSC::JIT::compileCTIMachineTrampolines):
-        * jit/JITCall.cpp:
-        (JSC::JIT::compileOpCall):
-        (JSC::JIT::compileOpCallSlowCase):
-        * jit/JITPropertyAccess.cpp:
-        (JSC::JIT::privateCompilePatchGetArrayLength):
-        * jit/JITStubs.cpp:
-        (JSC::JITStubs::JITStubs):
-        (JSC::JITStubs::tryCacheGetByID):
-        (JSC::JITStubs::cti_vm_dontLazyLinkCall):
-        (JSC::JITStubs::cti_op_get_by_val):
-        (JSC::JITStubs::cti_op_get_by_val_byte_array):
-        (JSC::JITStubs::cti_op_put_by_val):
-        (JSC::JITStubs::cti_op_put_by_val_array):
-        (JSC::JITStubs::cti_op_put_by_val_byte_array):
-        (JSC::JITStubs::cti_op_is_string):
-        * jit/JITStubs.h:
-        (JSC::JITStubs::ctiArrayLengthTrampoline):
-        (JSC::JITStubs::ctiStringLengthTrampoline):
-        (JSC::JITStubs::ctiVirtualCallPreLink):
-        (JSC::JITStubs::ctiVirtualCallLink):
-        (JSC::JITStubs::ctiVirtualCall):
-        * runtime/ArrayPrototype.cpp:
-        (JSC::arrayProtoFuncPop):
-        (JSC::arrayProtoFuncPush):
-        * runtime/FunctionPrototype.cpp:
-        (JSC::functionProtoFuncApply):
-        * runtime/JSArray.h:
-        (JSC::isJSArray):
-        * runtime/JSByteArray.h:
-        (JSC::asByteArray):
-        (JSC::isJSByteArray):
-        * runtime/JSCell.h:
-        * runtime/JSFunction.h:
-        * runtime/JSGlobalData.cpp:
-        (JSC::VPtrSet::VPtrSet):
-        (JSC::JSGlobalData::JSGlobalData):
-        (JSC::JSGlobalData::create):
-        (JSC::JSGlobalData::sharedInstance):
-        * runtime/JSGlobalData.h:
-        * runtime/JSString.h:
-        (JSC::isJSString):
-        * runtime/Operations.h:
-        (JSC::jsLess):
-        (JSC::jsLessEq):
-        * wrec/WREC.cpp:
-        (JSC::WREC::Generator::compileRegExp):
-
-
-
-git-svn-id: bf5cd6ccde378db821296732a091cfbcf5285fbd@41168 bbb929c8-8fbe-4397-9dbb-9b2b20218538"
+    text = """Update to optimize align/arrange buttons
 """.lower()
     print("is performance", is_performance(text))
     print("performance in text", re.findall(build_positive_regex(), text))
