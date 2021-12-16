11b22c84b83172b515a8137ce42dda037858a637
idanacumen <idan@acumen.io>
2021-12-16 13:42:40 +0200

    just todo and fixme
    exclude delete and get rid

--- satd_model.py
+++ satd_model.py
@@ -15,13 +15,18 @@ from model_evaluation import classifiy_commits_df, evaluate_performance, evaluat
 
 # Using the list from
 # An exploratory study on self-admitted technical debt by Potdar, Aniket and Shihab, Emad
-positive_terms = ['fixme', 'hack', 'todo', 'xxx']
+positive_terms = ['fixme'#, 'hack'
+                 , 'todo'
+                 #, 'xxx'
+                  ]
 
 removal_terms = [
     'because'
     , 'clean' + REGULAR_SUFFIX
+    , 'delet' + VERB_E_SUFFIX
     , 'fix' + REGULAR_SUFFIX
     , 'implement' + REGULAR_SUFFIX
+    , '(get|got|gets|getting)\srid'
     , 'list' + REGULAR_SUFFIX
     , 'mov' + VERB_E_SUFFIX
     , 'remov' + VERB_E_SUFFIX
@@ -45,8 +50,8 @@ def build_positive_regex():
 
 
 def build_excluded_regex():
-    return "(%s)" % ("|".join(excluded_terms))
-    #return build_sepereted_term(excluded_terms)
+    #return "(%s)" % ("|".join(excluded_terms))
+    return build_sepereted_term(excluded_terms)
 
 def build_not_positive_regex():
 
@@ -112,45 +117,14 @@ def evaluate_satd_classifier():
 
 
 if __name__ == '__main__':
-    print_concepts_functions_for_bq(commit='7bea7aa3de54a7763a52d93161615fe345483b19')
+    print_concepts_functions_for_bq(commit='1b9acf0a800300d27ea3b45be2239b44773bf897')
     evaluate_satd_classifier()
 
-    text = """""Merge branch 'for-linus' of git://git.infradead.org/users/vkoul/slave-dma
+    text = """
 
-Pull slave-dmaengine updates from Vinod Koul:
- ""For dmaengine contributions we have:
-   - designware cleanup by Andy
-   - my series moving device_control users to dmanegine_xxx APIs for
-     later removal of device_control API
-   - minor fixes spread over drivers mainly mv_xor, pl330, mmp, imx-sdma
-     etc""
-
-* 'for-linus' of git://git.infradead.org/users/vkoul/slave-dma: (60 commits)
-  serial: atmel: add missing dmaengine header
-  dmaengine: remove FSLDMA_EXTERNAL_START
-  dmaengine: freescale: remove FSLDMA_EXTERNAL_START control method
-  carma-fpga: move to fsl_dma_external_start()
-  carma-fpga: use dmaengine_xxx() API
-  dmaengine: freescale: add and export fsl_dma_external_start()
-  dmaengine: add dmaengine_prep_dma_sg() helper
-  video: mx3fb: use dmaengine_terminate_all() API
-  serial: sh-sci: use dmaengine_terminate_all() API
-  net: ks8842: use dmaengine_terminate_all() API
-  mtd: sh_flctl: use dmaengine_terminate_all() API
-  mtd: fsmc_nand: use dmaengine_terminate_all() API
-  V4L2: mx3_camer: use dmaengine_pause() API
-  dmaengine: coh901318: use dmaengine_terminate_all() API
-  pata_arasan_cf: use dmaengine_terminate_all() API
-  dmaengine: edma: check for echan->edesc => NULL in edma_dma_pause()
-  dmaengine: dw: export probe()/remove() and Co to users
-  dmaengine: dw: enable and disable controller when needed
-  dmaengine: dw: always export dw_dma_{en,dis}able
-  dmaengine: dw: introduce dw_dma_on() helper
-  ...
-"
 """.lower()
     print("Label", is_satd(text))
     print("concept in text", re.findall(build_positive_regex(), text))
     print("exclusion in text", re.findall(build_excluded_regex(), text))
-    #print(build_excluded_regex())
-    #print("v1", re.findall("(because|clean(?:s|ed|ing)?|fix(?:s|ed|ing)?|implement(?:s|ed|ing)?|list(?:s|ed|ing)?|remov(?:e|es|ed|ing)|was)[\S\s]{0,40}", text))
+    print(build_excluded_regex())
+    #print("v1", re.findall("(update todo|(because|clean(?:s|ed|ing)?|delet(?:e|es|ed|ing)|fix(?:s|ed|ing)?|implement(?:s|ed|ing)?|(get|got|gets|getting)\srid|list(?:s|ed|ing)?|mov(?:e|es|ed|ing)|remov(?:e|es|ed|ing)|resolv(?:e|es|ed|ing)|updat(?:e|es|ed|ing)|was))[\S\s]{0,40}", text))
